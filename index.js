ecfb5883c15a878051c8362adb0f0b0e3e11ec9a
jonathan-sh <jonathan.henrique.smtp@gmail.com>
2020-06-29 15:12:08 -0300

    adding history and setting focus at the last input

--- index.js
+++ index.js
@@ -1,37 +1,66 @@
-let input_id = 0;
+let inputId = 0;
+let history = [];
+let historyIndex = 0;
+
+
+const setFocusAtTheLastInput = () => {
+    document.getElementById(inputId).focus();
+};
+
 const printResult = () => {
 
-    const command = document.getElementById(input_id).value;
-    
+    const command = document.getElementById(inputId).value;
+
     let output = `bash: ${command}: command not found...`;
 
-    if(command === "date") output = new Date()
+    if (command === "date") output = new Date().toString();
 
-    if(command!==""){
+    if (command !== "") {
+        history.push(command);
         const line = `<span class="output">${output}</span>`;
         document.getElementById("terminal").insertAdjacentHTML('beforeend', line);
     }
-    
-    document.getElementById(input_id).focus();
 
+    setFocusAtTheLastInput();
 }
 
 const newLine = () => {
-    document.getElementById(input_id).disabled = true;
+    document.getElementById(inputId).disabled = true;
     const line = `<input-line style="display: inline-block; width: 100%;">
                     <span class="name">headhunter@jarvis:~$&nbsp;</span>
-                    <input id ="${++input_id}"class="input" onkeypress="onEnter(event)">">
+                    <input id ="${++inputId}"class="input" onkeyup="pressKey(event)">
                   </input-line>`;
     document.getElementById("terminal").insertAdjacentHTML('beforeend', line);
-    document.getElementById(input_id).focus();
+    setFocusAtTheLastInput();
 };
 
+const getHistory = () => {
+    if (history.length > 0) {
+        let element = document.getElementById(inputId);
+        element.value = history[historyIndex];
+    }
 
-let onEnter = (it) => {
+    if (historyIndex != 0) {
+        --historyIndex;
+    }
+};
+
+const resetHistoryIndex = () => {
+    historyIndex = history.length
+};
+
+const pressKey = (it) => {
     if (it.which == 13 || it.keyCode == 13) {
+        resetHistoryIndex();
         printResult();
         newLine();
         return false;
+
     }
+    else if (it.which == 38 || it.keyCode == 38) {
+        getHistory();
+        return false;
+    }
+
     return true;
 }
